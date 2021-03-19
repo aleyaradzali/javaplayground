d9ef3f8c538a1bbfb87c0a3da0d8b15c29086d73
Aizadtul Aleya <aizadtulaleya@gmail.com>
2021-03-19 11:50:15 +0800

    third commit

--- samples/src/samples/GitTest.java
+++ samples/src/samples/GitTest.java
@@ -2,19 +2,6 @@ package samples;
 
 class GitTest  {
 
-	public class Location {
-		private String var;
-
-		public String getVar() {
-			return var;
-		}
-
-		public void setVar(String var) {
-			this.var = var;
-		}
-		
-	}
-
 	public static void main(String[] args) {
 		System.out.println("Git is fun");
 		System.out.println("Git is cool");
