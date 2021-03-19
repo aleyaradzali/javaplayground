4e52dd1edc5495c157ba80f35908fe9a81346775
Aizadtul Aleya <aizadtulaleya@gmail.com>
2021-03-19 11:54:39 +0800

    Location commit

---
+++ samples/src/samples/Location.java
@@ -0,0 +1,15 @@
+package samples;
+
+public class Location {
+	private String var;
+
+	public String getVar() {
+		return var;
+	}
+
+	public void setVar(String var) {
+		this.var = var;
+	}
+	
+}
+
